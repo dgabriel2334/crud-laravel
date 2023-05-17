bb7804fe70cda9bd11f5314a01af93c9d248332d
Dário Gabriel <dario.santos@westwing.com.br>
2023-05-17 00:28:23 -0300

    Add new features

---
+++ app/Http/Controllers/VeiculosController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Response;
+use Illuminate\Http\Request;
+use App\Models\Veiculos\ModelVeiculos;
+use App\Models\Veiculos\ModelVeiculosCategoria;
+
+class VeiculosController extends Controller
+{
+    protected $_objVeiculo;
+    protected $_objVeiculoCategoria;
+
+    public function __construct()
+    {
+        $this->_objVeiculo = new ModelVeiculos();
+        $this->_objVeiculoCategoria = new ModelVeiculosCategoria();
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $sortBy = $request->input('sortBy') ?? 'id';
+        $categorias = $this->_objVeiculoCategoria->all()->sortBy($sortBy);
+
+        return view('categorias/index', compact('categorias'));
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        if ($request->hasFile('imagem')) {
+            $imagem = $request->file('imagem');
+            $imagem->move('img/upload', $imagem->hashName());
+        }
+        exit;
+
+        $this->_objVeiculo->create([
+            'name' => $request->veiculo_nome,
+            'montadora' => $request->montadora,
+            'descricao' => $request->descricao,
+            'observacoes' => $request->observacoes,
+            'fk_categoria' => $request->categoria,
+            'fk_categoria' => $request->categoria,
+            // 'image' => $request->image,
+        ]);
+
+
+        return Response::json(['status' => 'success', 'message' => 'Criado com sucesso!']);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $categoria = $this->_objVeiculoCategoria->find($id);
+
+        return Response::json([
+            'status' => 'success',
+            'categoria_id' => $categoria->id,
+            'categoria_nome' => $categoria->name,
+            'categoria_vl_hora' => $categoria->vl_hora,
+            'categoria_vl_diaria' => $categoria->vl_diaria,
+            'categoria_vl_semana' => $categoria->vl_semana,
+            'categoria_vl_mes' => $categoria->vl_mes,
+            'message' => ''
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $this->_objVeiculoCategoria->where(['id' => $id])->update([
+            'name' => $request->name,
+            'vl_hora' => $request->valor_hora,
+            'vl_diaria' => $request->valor_diaria,
+            'vl_mes' => $request->valor_mes,
+            'vl_semana' => $request->valor_semana
+        ]);
+
+        return Response::json(['status' => 'success', 'message' => 'Criado com sucesso!']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        if (count($this->_objVeiculo->where('fk_categoria', $id)->get()) > 0) {
+            return Response::json(['status' => 'error', 'message' => 'Erro ao apagar! Existem veículos nesta categoria, considere apagar os veículos primeiro!']);
+        }
+
+        if (!$this->_objVeiculoCategoria->destroy($id)) {
+            return Response::json(['status' => 'error', 'message' => 'Erro ao apagar']);
+        }
+
+        return Response::json(['status' => 'success', 'message' => 'Apagado com sucesso!']);
+    }
+
+    /**
+     * retorna o valor float formatado em real.
+     */
+    public static function moedaToBr(float $valor): string {
+        $valorFormatado = number_format($valor, 2, ',', '.');
+        
+        $valorFormatado = 'R$ ' . $valorFormatado;
+        
+        return $valorFormatado;
+    }
+    
+}
